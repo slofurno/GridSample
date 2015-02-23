efb2b9b061554d4f70c2b311c7c4ff42dac540b9
Heath Yates <hlyates@ksu.edu>
2015-02-23 11:27:20 -0600

    Initial commit
    ASP.NET 4.5 MVC5 Application demonstrating grid samples.

---
+++ Controllers/GridController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GridSample.Controllers
+{
+    public class GridController : Controller
+    {
+     
+        public ActionResult GridOne()
+        {
+            return View();
+        }
+
+        public ActionResult GridTwo()
+        {
+            return View();
+        }
+    }
+}
