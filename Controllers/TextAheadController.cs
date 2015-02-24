2a3772e2224efa2b92cdc5dd0d28c264147d53f0
Heath Yates <hlyates@ksu.edu>
2015-02-24 15:35:19 -0600

    Textahead added. Not working.

---
+++ Controllers/TextAheadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GridSample.Controllers
+{
+    public class TextAheadController : Controller
+    {
+        // GET: TextAhead
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
